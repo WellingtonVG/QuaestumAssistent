ee334c2d2e27c7a69d68583e9e44f3b0682d6890
Wellington Velasco Geronymo <wellingtoon.vg@outlook.com>
2020-06-07 21:46:50 -0300

    firt commit

---
+++ QA2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Globalization;
+
+namespace QA2
+{
+    public partial class Form1 : Form
+    {
+        static CultureInfo ci = new CultureInfo("pt-BR");
+        static SpeechRecognitionEngine reconhecedor;
+        SpeechSynthesizer resposta = new SpeechSynthesizer();
+
+        public string[] listaPalavras = { "oi", "quem é você", "como você está", "quaestum" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //this.WindowState = FormWindowState.Minimized;
+            //this.ShowInTaskbar = false;
+
+            Init();
+        }
+
+
+        public void Gramatica()
+        {
+            try
+            {
+                reconhecedor = new SpeechRecognitionEngine(ci);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao integrar Lingua escolhoida" + ex.Message);
+            }
+
+            var gramatica = new Choices();
+            gramatica.Add(listaPalavras);
+
+            var gb = new GrammarBuilder();
+            gb.Append(gramatica);
+
+            try
+            {
+                var g = new Grammar(gb);
+
+                try
+                {
+                    reconhecedor.RequestRecognizerUpdate();
+                    reconhecedor.LoadGrammarAsync(g);
+                    reconhecedor.SpeechRecognized += Sre_Reconhecimento;
+                    reconhecedor.SetInputToDefaultAudioDevice();
+                    resposta.SetOutputToDefaultAudioDevice();
+                    reconhecedor.RecognizeAsync(RecognizeMode.Multiple);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao criar gramática: " + ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao criar reconhecedor:  " + ex.Message);
+            }
+        }
+        public void Init()
+        {
+            resposta.Volume = 75;
+            resposta.Rate = 2;
+
+            Gramatica();
+        }
+
+        void Sre_Reconhecimento(object sender, SpeechRecognizedEventArgs e)
+        {
+            string frase = e.Result.Text;
+
+            if (frase.Equals("oi"))
+            {
+                resposta.SpeakAsync("Oi, como você está ?");
+            }
+            else if (frase.Equals("quaestum"))
+            {
+                this.Alert("Quaestum");
+            }
+        }
+
+        public QuaestumAssistent frm;
+
+        public void Alert(string msg)
+        {
+            this.frm = new QuaestumAssistent( );
+            this.frm.showAlert(msg);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Alert("Quaestum");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.frm.comando("painel");
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Erro:  " + ex.Message);
+            }
+        }
+    }
+}
---
+++ QA2/QuaestumAssistent.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace QA2
+{
+    partial class QuaestumAssistent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuaestumAssistent));
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.ForeColor = System.Drawing.Color.White;
+            this.lblMsg.Location = new System.Drawing.Point(161, 29);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(136, 30);
+            this.lblMsg.TabIndex = 0;
+            this.lblMsg.Text = "Comando";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // QuaestumAssistent
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(378, 91);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblMsg);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "QuaestumAssistent";
+            this.Text = "Quaestum Assistent";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
